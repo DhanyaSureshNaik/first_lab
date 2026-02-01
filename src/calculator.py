5465c34ff5f76825083a0628f05a2e627fa441f7
Dhanya S Naik <dhanyasureshnaik@icloud.com>
2026-02-01 01:26:42 -0500

    Added src and test

---
+++ src/calculator.py
@@ -0,0 +1,17 @@
+def fun1(x, y):
+    return x + y
+
+
+def fun2(x, y):
+    return x - y
+
+
+def fun3(x, y):
+    return x * y
+
+
+def fun4(x, y):
+    result1 = fun1(x, y)
+    result2 = fun2(x, y)
+    result3 = fun3(x, y)
+    return result1 + result2 + result3
